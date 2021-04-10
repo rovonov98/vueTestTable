d3b13b4562548e2b6c0eaace392734e11ad57f14
rovonov98 <stariyakk121@gmail.com>
2021-04-10 19:50:54 +0500

    webpack config changes

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = (_, { mode }) => ({
     entry: {
         main: path.join(__dirname, 'src', 'main.js'),
-        vendor: path.join(__dirname, 'src', 'vendor.js')
+        // vendor: path.join(__dirname, 'src', 'vendor.js')
     },
     output: {
         path: path.join(__dirname, 'dist'),
